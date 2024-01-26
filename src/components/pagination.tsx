1cb4d04079c5f78a16a8ee293a5b8b279c99b319
joaovcostas <jvc22102003@gmail.com>
2024-01-26 13:24:27 -0300

    itens(s) to item(s)

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -22,7 +22,7 @@ export function Pagination({
   return (
     <div className="flex items-center justify-between">
       <span className="text-sm text-muted-foreground">
-        Total de {totalCount} itens(s)
+        Total de {totalCount} item(s)
       </span>
 
       <div className="flex items-center gap-6 lg:gap-8">
